6eacc5000e771b7f25080b4d83cceafabd2a893a
robertop96 <roberto.manzano96@gmail.com>
2021-04-02 13:45:57 -0400

    For pull purpose
    Multi-line description of commit,
    feel free to be detailed.
    
    [Ticket: X]

--- client/src/pages/Crime/Crime.jsx
+++ client/src/pages/Crime/Crime.jsx
@@ -2,53 +2,29 @@ import React, { useState, useEffect } from 'react';
 import Typography from '@material-ui/core/Typography';
 import SimpleCard from '../../components/Card/Card';
 import axios from 'axios';
-import Header from '../../components/Header/Header';
-import CrimeList from '../../components/CrimeList/CrimeList';
-
-
 
 const Crime = () => {
-  const [criminalList, setCriminalList] = useState();
+  //const [criminal, setCriminal] = useState();
 
   useEffect(() => {
     axios
       .get('/api/crimes')
       .then((response) => {
-        // console.log(response);
-        setCriminalList(response.data);
+        console.log(response);
       })
       .catch((error) => {
         console.log(error);
       });
   }, []);
 
-  //console.log(criminalList);
   return (
-    <>
-      {criminalList && (
-        <>
-          <section>
-            <Header />
-            <h1>Crime Page</h1>
-            <Typography> Crime page</Typography>
-
-            <article className="cardContainer">
-              {criminalList.map((criminal) => {
-
-                return <SimpleCard name={criminal.name} count={criminal.count} />
-              })}
-            </article>
-            <div className="crime__list"></div>
-          </section>
-        </>
-      )
-      }
-    </>
-
+    <section>
+      <h1>Crime Page</h1>
+      <Typography> Crime page</Typography>
+      <SimpleCard />
+      <div className="crime__list"></div>
+    </section>
   );
-
 };
-
-
 
 export default Crime;
