12327a12194a0f15e7d15e0cba5931a595b6bb26
xfroggy <tc.smitala@gmail.com>
2021-04-02 13:36:20 -0400

    try to add crime component again

---
+++ client/src/pages/Crime-old/Crime-old.jsx
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+import Typography from '@material-ui/core/Typography';
+import SimpleCard from '../../components/Card/Card';
+import axios from 'axios';
+import Header from '../../components/Header/Header';
+import CrimeList from '../../components/CrimeList/CrimeList';
+
+
+
+const Crime = () => {
+  const [criminalList, setCriminalList] = useState();
+
+  useEffect(() => {
+    axios
+      .get('/api/crimes')
+      .then((response) => {
+        // console.log(response);
+        setCriminalList(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  //console.log(criminalList);
+  return (
+    <>
+      {criminalList && (
+        <>
+          <section>
+            <Header />
+            <h1>Crime Page</h1>
+            <Typography> Crime page</Typography>
+
+            <article className="cardContainer">
+              {criminalList.map((criminal) => {
+
+                return <SimpleCard name={criminal.name} count={criminal.count} />
+              })}
+            </article>
+          </section>
+        </>
+      )
+      }
+    </>
+
+  );
+
+};
+
+
+
+export default Crime;
